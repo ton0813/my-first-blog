49b9045b8d658e02073bb6c8fe0671329761c56f
Your Name <you@example.com>
2021-10-09 14:48:43 +0900

    databases change

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,4 +115,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env)
+
+DATABASES['default'].update(db_from_env)
