e08f890b59f381d3bb6c2aa9368695e8d6b559cb
Your Name <you@example.com>
2021-10-12 17:42:30 +0900

    add drart

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,6 @@ def post_new(request):
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            
             post.save()
             return redirect('post_detail', pk=post.pk)    
     else:
@@ -34,7 +33,6 @@ def post_edit(request,pk):
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -44,4 +42,8 @@ def post_edit(request,pk):
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
-    
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
